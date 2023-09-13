dc7d562a105eff9538ebf88d7f5949032d1d1013
VikVanDik <95vlc17@gmail.com>
2023-09-13 16:00:38 +0200

    [add]: markup, style and starting script

---
+++ script/script.js
@@ -0,0 +1,5 @@
+//2. Creare l'array con le immagini all'interno.
+
+const img = ['./img/01.webp','./img/02.webp','./img/03.webp','./img/04.webp','./img/05.webp']
+
+// 3. Collegare l'array ad un ciclo così da ottenere l'elemento che cicla.
